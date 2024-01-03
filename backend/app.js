9eafac1956ae85ecd30ce0f354a19ca4a6a2e8e0
supzi-del <78655439+supzi-del@users.noreply.github.com>
2024-01-03 20:37:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ connectDB();
 
 const allowedOrigins = [
   "https://main.d1sj7cd70hlter.amplifyapp.com",
-  "https://expense-tracker-app-main.vercel.app",// Add the local development origin
+  "https://expense-tracker-app-main-git-main-suprithas-projects-eb55b552.vercel.app",// Add the local development origin
   // add more origins as needed
 ];
 
