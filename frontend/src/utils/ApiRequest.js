3a1c51996db989b6681826b8948da8add47df1c0
supzi-del <78655439+supzi-del@users.noreply.github.com>
2024-01-03 20:08:13 +0530

    Update ApiRequest.js

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,5 +1,6 @@
 //const host = "https://expense-tracker-app-knl1.onrender.com";
 const host = "https://ckrhldi0i7.execute-api.eu-north-1.amazonaws.com/dev";
+
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
